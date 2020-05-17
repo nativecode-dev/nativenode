f908058fd982e078ffc3463b41f2c63451277060
NativeCode <opensource@nativecode.com>
2020-05-17 16:23:30 -0400

    feat(moving scheduling and queuing): moved scheduling and queuing to core-queue
    affects: @nnode/core-queue, @nnode/core-web, @nnode/core
    
    BREAKING CHANGE:
    moved scheduling and queuing to core-queue

--- packages/core-web/src/ServerConfig.ts
+++ packages/core-web/src/ServerConfig.ts
@@ -3,6 +3,8 @@ import { Config } from '@nnode/core'
 export interface ServerConfig extends Config {
   enableSessions: boolean
   enableWebSockets: boolean
+  host: string
+  port: number
   statics: string[]
 }
 
