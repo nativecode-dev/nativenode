d367caf5880c016e92ae6b430b3a8d1bfca7b62c
NativeCode <opensource@nativecode.com>
2020-06-05 10:45:18 -0400

    fix: RouteDefine root route

--- packages/core-web/src/RouteDefine.ts
+++ packages/core-web/src/RouteDefine.ts
@@ -6,6 +6,6 @@ export class RouteDefine {
   constructor(private readonly type: string, private readonly router: Express) {}
 
   define(route: string): RouteVerb {
-    return new RouteVerb(this, `${this.type}${route}`, this.router)
+    return new RouteVerb(this, `/${this.type}${route}`, this.router)
   }
 }
